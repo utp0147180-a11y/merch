8188703535aba610edfe5c5560376966ef9b2e89
utp0147180-a11y <utp0147180@alumno.utpuebla.edu.mx>
2026-06-24 12:26:03 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 import Orders from './pages/Admin/Orders';
+import Products from './pages/Admin/Products';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/admin/orders" element={<Orders />} />
+      <Route path="/admin/products" element={<Products />} />
     </Routes>
   );
 }
